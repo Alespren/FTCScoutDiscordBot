698dfd593d4eb1d30c40ddc5fb5f1667bff8509e
Alespren <corbinped@gmail.com>
2024-02-11 16:56:53 -0700

    Put data into an embed

--- commands/get-team.js
+++ commands/get-team.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js')
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,6 +10,8 @@ module.exports = {
 				.setDescription('The team number of the team you want the data for.')
 		),
 	async execute(interaction) {
+		await interaction.deferReply()
+
 		const teamNumber = interaction.options.getString('team-number')
 
 		const response = await fetch(
@@ -17,6 +19,36 @@ module.exports = {
 		)
 		const data = await response.json()
 
-		console.log(data)
+		const quickStatsResponse = await fetch(
+			`https://api.ftcscout.org/rest/v1/teams/${teamNumber}/quick-stats`
+		)
+		const stats = await quickStatsResponse.json()
+
+		// Construct embed
+		const embed = new EmbedBuilder()
+			.setColor('#fee066')
+			.setTitle(`${data.number} ${data.name}`)
+			.setURL(`https://ftcscout.org/teams/${data.number}`)
+			.addFields(
+				{ name: 'School', value: `${data.schoolName}`, inline: true },
+				{
+					name: 'Sponsors',
+					value: `${data.sponsors.join(', ')}`,
+					inline: true,
+				},
+				{ name: '\u200B', value: '\u200B', inline: true },
+				{
+					name: 'Location',
+					value: `${data.city}, ${data.state}, ${data.country}`,
+					inline: true,
+				},
+				{ name: 'Rookie Year', value: `${data.rookieYear}`, inline: true },
+				{ name: '\u200B', value: '\u200B', inline: true }
+			)
+			.setFooter({
+				text: `Updated ${new Date(data.updatedAt).toDateString()}`,
+			})
+
+		interaction.followUp({ embeds: [embed] })
 	},
 }
