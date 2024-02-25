831a3fc15c724da4d974616e4764c4f67d965e93
Alespren <corbinped@gmail.com>
2024-02-25 13:39:12 -0700

    Switch to ESM

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,33 +1,42 @@
-const { REST, Routes } = require('discord.js')
-const fs = require('node:fs')
-const path = require('node:path')
-require('dotenv').config()
+import { REST } from '@discordjs/rest'
+import { Routes } from 'discord.js'
+import dotenv from 'dotenv'
+import fs from 'fs'
+import path from 'path'
+import url from 'url'
+
+dotenv.config()
 
 const commands = []
+// Grab all the command folders from the commands directory you created earlier
+const foldersPath = path.join(import.meta.dirname, 'commands')
+const commandFolders = fs.readdirSync(foldersPath)
 
-// Grab all the command files from the commands directory you created earlier
-const commandsPath = path.join(__dirname, 'commands')
-const commandFiles = fs
-	.readdirSync(commandsPath)
-	.filter((file) => file.endsWith('.js'))
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file)
-	const command = require(filePath)
-	if ('data' in command && 'execute' in command) {
-		commands.push(command.data.toJSON())
-	} else {
-		console.log(
-			`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-		)
+for (const folder of commandFolders) {
+	// Grab all the command files from the commands directory you created earlier
+	const commandsPath = path.join(foldersPath, folder)
+	const commandFiles = fs
+		.readdirSync(commandsPath)
+		.filter((file) => file.endsWith('.js'))
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file)
+		const commandModule = await import(url.pathToFileURL(filePath))
+		const command = commandModule.default
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON())
+		} else {
+			console.log(
+				`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+			)
+		}
 	}
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST().setToken(process.env.DISCORD_TOKEN)
-
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 // and deploy your commands!
-;(async () => {
+(async () => {
 	try {
 		console.log(
 			`Started refreshing ${commands.length} application (/) commands.`
@@ -35,10 +44,7 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN)
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(
-				process.env.CLIENT_ID,
-				process.env.GUILD_ID
-			),
+			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
 			{ body: commands }
 		)
 
--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-const fs = require('node:fs')
-const path = require('node:path')
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js')
-require('dotenv').config()
+import fs from 'fs'
+import path from 'path'
+import url from 'url'
+import { Client, Collection, Events, GatewayIntentBits } from 'discord.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
 client.commands = new Collection()
 
-const commandsPath = path.join(__dirname, 'commands')
-const commandFiles = fs
-	.readdirSync(commandsPath)
-	.filter((file) => file.endsWith('.js'))
+const foldersPath = path.join(import.meta.dirname, 'commands')
+const commandFolders = fs.readdirSync(foldersPath)
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file)
-	const command = require(filePath)
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command)
-	} else {
-		console.log(
-			`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-		)
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder)
+	const commandFiles = fs
+		.readdirSync(commandsPath)
+		.filter((file) => file.endsWith('.js'))
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file)
+		const commandModule = await import(url.pathToFileURL(filePath))
+		const command = commandModule.default
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command)
+		} else {
+			console.log(
+				`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+			)
+		}
 	}
 }
 
